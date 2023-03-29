9bf4c24707d1cf62a5dec7d9c74e1c381a2bd26b
aleksey.krasnov <krasnov.80@mail.ru>
2023-03-29 11:58:53 +0300

    Taskadd1 done

--- Taskadd1/Program.cs
+++ Taskadd1/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Транспонировать относительно горизонтали
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(1, 11);
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+
+void TransposeMatrix(int[,] matrix,int [,] tMatrix) //Транспонировать
+{
+    int l = 0;
+    for (int i = matrix.GetLength(0) - 1; i >= 0; i--)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            tMatrix[l, j] = matrix[i, j];
+        }
+        l++;
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размерность массива: ");
+int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+int[,] tMatrix=new int[size[0],size[1]];
+InputMatrix(matrix);
+Console.WriteLine("Начальный массив");
+PrintMatrix(matrix);
+Console.WriteLine("Новый массив");
+TransposeMatrix(matrix, tMatrix);
+PrintMatrix(tMatrix);

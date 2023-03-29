9d28dd165afe09823ed5f953b2c3a6178aec5a77
aleksey.krasnov <krasnov.80@mail.ru>
2023-03-29 13:56:00 +0300

    Task47 done v2

--- Task47/Program.cs
+++ Task47/Program.cs
@@ -4,7 +4,7 @@ void InputMatrix(double[,] matrix)
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
-            matrix[i, j] = Math.Round (new Random().NextDouble()*10,2);
+            matrix[i, j] = Math.Round (new Random().Next(-100,101)*0.1,2);
     }
 }
 

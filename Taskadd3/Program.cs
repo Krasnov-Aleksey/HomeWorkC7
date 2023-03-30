70f6ad80fd44e5f54ebfed021d460851ee5bad80
aleksey.krasnov <krasnov.80@mail.ru>
2023-03-30 13:54:13 +0300

    Taskadd 3 done

--- Taskadd3/Program.cs
+++ Taskadd3/Program.cs
@@ -1,36 +1,38 @@
-﻿
+﻿//Заполнение диоганалями
 void InputMatrix(int[,] matrix)
 {
-    // matrix[0,0]=0;
-    // matrix[0,1]=1;
-    // matrix[1,0]=matrix[0,1]+1;//2
-    // matrix[0,2]=matrix[1,0]+1;//3
-    // matrix[1,1]=matrix[0,2]+1;//4
-    int n = 2;
+    int n = 0;
     matrix[0, 0] = 0;
     int a = matrix[0, 0];
     for (int i = 1; i < matrix.GetLength(1); i++)
     {
-        n=i;
+        n = i;
         for (int j = 0; j < matrix.GetLength(0); j++)
         {
             if (n >= 0)
             {
                 matrix[j, n] = a + 1;
                 a = matrix[j, n];
-                n = n - 1;
+                n--;
             }
-
-            // n=n-1;
-            // matrix[i,n]=a+1;
-            // a=matrix[i,n];
         }
     }
-    //     for (int i = 0; i < matrix.GetLength(0); i++)
-    //     {
-    //         for (int j = 1; j < matrix.GetLength(1); j++)
-    //             matrix[i, j] = new Random().Next(-10, 11);
-    //     }
+    for (int i = 1; i < matrix.GetLength(0); i++)
+    {
+        n = i;
+        for (int j = matrix.GetLength(1) - 1; j >= 0; j--)
+        {
+            //Console.WriteLine(j);
+            if (n <matrix.GetLength(0))
+            {
+                //Console.Write(n);
+                //Console.WriteLine(j);
+                matrix[n, j] = a + 1;
+                a = matrix[n, j];
+                n++;
+            }
+        }
+    }
 }
 
 void PrintMatrix(int[,] matrix)
@@ -44,9 +46,8 @@ void PrintMatrix(int[,] matrix)
 }
 
 Console.Clear();
-Console.WriteLine("Введите размерность массива: ");
-//int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
-//int[,] matrix = new int[size[0], size[1]];
-int[,] matrix = new int[4, 4];
+Console.Write("Введите размерность массива: ");
+int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
 InputMatrix(matrix);
 PrintMatrix(matrix);
